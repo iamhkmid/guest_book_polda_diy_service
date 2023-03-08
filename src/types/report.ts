5286f234a7f9308f26d1d860053bb5e298c0539b
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2023-03-08 22:40:08 +0700

    update login

--- src/types/report.ts
+++
@@ -1,5 +0,0 @@
-
-export type TGenerateTable = (p: {
-  doc: PDFKit.PDFDocument,
-  data:  { no: number; name: string; phone: string; address: string; description: string; visitDate: Date }[]
-}) => void
